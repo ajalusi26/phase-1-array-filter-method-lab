50d39d319c2f9ee461add31948fb975d6d08995f
AJ Jalusi <ajalusi26@gmail.com>
2021-12-16 10:33:49 -0500

    commit tree f50ae96743d0c7da92ac116944de690719cf1c87

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name){
+    return drivers.filter(ppl => ppl.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(drivers, letter){
+    return drivers.filter(ppl => ppl[0] + ppl[1] === letter)
+}
+function matchName(drivers, string){
+    return drivers.filter(ppl => ppl.name === string)
+}
